f0d4277f2d689352e7043b72cd9aca8a66fbf0fb
defdalila <defdalila@outlook.com>
2025-03-20 00:57:37 -0300

    feat(contato): add CRUD operations for Contato entity

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -34,4 +34,67 @@ public class ContatoController : ControllerBase
         return Ok(null);
     }
     
+    /// <summary>
+    /// Retorna um contato pelo seu id
+    /// </summary>
+    /// <param name="id">id do contato</param>
+    /// <returns>contato caso encontrado, NotFound() caso n o encontrado</returns>
+    [HttpGet("{id}")]
+    public IActionResult Read(int id)
+    {
+        var contato = _context.Contatos.Find(id);
+        
+        if (contato == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(contato);
+    }
+    
+    /// <summary>
+    /// Atualiza um contato
+    /// </summary>
+    /// <param name="id">id do contato</param>
+    /// <param name="contato">dados do contato a ser atualizado</param>
+    /// <returns>Not Found() caso n o encontrado, Ok() caso encontrado</returns>
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Contato contato)
+    {
+        var data = _context.Contatos.Find(id);
+        
+        if (data == null)
+        {
+            return NotFound();
+        }
+        
+        data.Nome = contato.Nome;
+        data.Telefone = contato.Telefone;
+        data.Email = contato.Email;
+        data.Ativo = contato.Ativo;
+        
+        _context.SaveChanges();
+        return Ok(null);
+    }
+    
+    /// <summary>
+    /// Remove um contato
+    /// </summary>
+    /// <param name="id">id do contato</param>
+    /// <returns>NotFound() caso n o encontrado, Ok() caso encontrado</returns>
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var contato = _context.Contatos.Find(id);
+        
+        if (contato == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Contatos.Remove(contato);
+        _context.SaveChanges();
+        return Ok(null);
+    }
+    
 }
